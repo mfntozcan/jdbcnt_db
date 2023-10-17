58b0fa0470ac031821ca3ac5c5bef6831c6bb6ca
mfntozcan@gmail.com <mfntozcan@gmail.com>
2023-10-17 21:50:36 +0200

    transactioan

--- src/main/java/Transaction01.java
+++ src/main/java/Transaction01.java
@@ -2,13 +2,39 @@
 /*
 Transaction ;; DataBase deki en kucuk(parcalanamaz, atomik) islemdir.
 
+Birden fazla islemi gruplayarak tek bir transaction olusturabiliriz.
+
+Bu islemlerden en az biri basarisiz olursa ROLLBACK ile diger islemler de iptal edilerek veri tutarliligi saglanir.
+
+Bu islemlerin tamami basarili olursa degisiklikler COMMIT ile onaylayarak DB de kalici hale getirilir.
+
 
  */
 
+import java.sql.*;
 
 public class Transaction01 {
+    public static void main(String[] args) throws SQLException {
 
-    public static void main(String[] args) {
+        //hesap no:1234 ten hesap no:5678 e 1000$ para transferi olsun.
+
+        Connection con = DriverManager.
+                getConnection("jdbc:postgresql://localhost:5432/jdbcnt_db","techpront","password");
+
+        Statement st =con.createStatement();
+
+        String sql = "UPDATE hesaplar SET bakye=bakiye+? WHERE hesap_no=?";
+        PreparedStatement prst = con.prepareStatement(sql);
+
+        //1- para transferi yapan hesabin bakiyesini guncellenecek
+        prst.setInt(1,-1000);
+        prst.setInt(2,1234);
+        prst.executeUpdate();
+
+        //2- para transferi alan hesabin bakiyesini guncellencek
+        prst.setInt(1,1000);
+        prst.setInt(2,5678);
+        prst.executeUpdate();
 
 
 
