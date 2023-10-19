f90e90695710908763118b62b7414e2f8dd1e5db
mfntozcan@gmail.com <mfntozcan@gmail.com>
2023-10-19 21:37:01 +0200

    med

--- src/test/java/MedunnaTest.java
+++ src/test/java/MedunnaTest.java
@@ -1,3 +1,18 @@
 public class MedunnaTest {
 
+      /*
+   Given
+     Kullanıcı veritabanına bağlanır
+     (Host name: medunna.com, Database name: medunna_db_v2, Username: select_user, Password: Medunna_pass_@6))
+
+   When
+     Kullanıcı, oluşturulan odayı getirmek için room_number ile sorgu gönderir
+
+   Then
+     Kullanıcı, oda bilgilerinin doğru kaydedildiğini doğrular
+
+   And
+     Kullanıcı, bağlantıyı kapatır
+  */
+
 }
